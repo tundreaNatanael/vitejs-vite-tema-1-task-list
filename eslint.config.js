4b3f080c5186e9773324898e4a7af4e7cac76f61
tundreaNatanael <tundrea.natanael.work@gmail.com>
2025-03-07 10:30:20 +0200

    added: lint added

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,41 @@
 import globals from "globals";
-import pluginJs from "@eslint/js";
-import pluginVue from "eslint-plugin-vue";
-
+import js from "@eslint/js";
+import vue from "eslint-plugin-vue";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,vue}"]},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  ...pluginVue.configs["flat/essential"],
+  {
+    ignores: [
+      "**/node_modules/**",
+      "dist/**",
+      "build/**",
+      "coverage/**",
+      "*.json",
+      "*.lock",
+      "*.md",
+      "*.yaml",
+      "*.yml",
+    ],
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,vue}"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...vue.configs["flat/essential"].rules,
+      "no-alert": "error",
+      "no-confirm": "error",
+      "no-prompt": "error",
+      "quotes": ["error", "single"],
+      "max-len": ["error", { code: 80 }],
+      "no-trailing-spaces": "error",
+      "no-var": "error",
+      "eol-last": ["error", "always"],
+      "vue/multi-word-component-names": "off"
+    },
+  },
 ];
